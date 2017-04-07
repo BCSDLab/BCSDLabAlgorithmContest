9485c47f1c8f5617e2790610b814c465483a6f69
ChanHyeok Jeong <tkdahr16@gmail.com>
2017-04-07 20:37:07 +0900

    Add files via upload

--- bcsd_test/src/bcsd_test/main1.java
+++ bcsd_test/src/bcsd_test/main1.java
@@ -6,12 +6,13 @@ public class main1 {
 	// 124 이하의 수에서 뒤에 연속되는 0의 수를 구하는 메서드
 	public static void main(String[] args) {
 		int num = 100;
-		
+		int sum = 0;
 		Scanner in = new Scanner(System.in);
 		
 		num = in.nextInt();
 		if(num <= 0 && num >= 125)
-			System.out.println("-1");
-		System.out.println((num/25 + num/5)+1);
+			sum = -1;
+		sum += 	num / 125+num/25 + num/5;
+		System.out.println(sum);
 	}
 }
