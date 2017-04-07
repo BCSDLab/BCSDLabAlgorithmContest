9fed80293c55695728ea9dfcb50fbe26f0a1fc7a
ChanHyeok Jeong <tkdahr16@gmail.com>
2017-04-07 20:44:32 +0900

    Add files via upload

--- bcsd_test/src/bcsd_test/main1.java
+++ bcsd_test/src/bcsd_test/main1.java
@@ -12,7 +12,7 @@ public class main1 {
 		num = in.nextInt();
 		if(num <= 0 && num >= 125)
 			sum = -1;
-		sum += 	num / 125+num/25 + num/5;
+		sum +=num/1220703125+num/244140625+num/48828125+num/9765625+ num / 1953125+num/390625  + num/78125 + num / 15625 +num / 3125 + num / 125 + num / 625 + num/25 + num/5;
 		System.out.println(sum);
 	}
 }
